37757043a2e0b203b255e0f5cbfd10cf44eae13d
Gevorg Nersisyan <56558019+Gevorg03@users.noreply.github.com>
2022-08-12 01:28:21 +0400

    Update remove_duplicates(26).py

--- day2/remove_duplicates(26).py
+++ day2/remove_duplicates(26).py
@@ -1,4 +1,9 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        nums = list(set(nums))
+        count = 0
+        for i in range(len(nums)):
+            if nums.count(nums[count]) > 1:
+                del nums[nums.index(nums[count])]
+            else:
+                count += 1
         return len(nums)  
